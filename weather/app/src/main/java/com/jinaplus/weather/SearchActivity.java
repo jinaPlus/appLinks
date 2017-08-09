c31b23a4cabd0c30c1d75b5d1d038c3dbc431ec4
안진아(jina.ahn)/App개발팀/SKP <jin.a.ahn@sk.com>
2017-08-09 18:14:09 +0900

    App-link test completed.

--- weather/app/src/main/java/com/jinaplus/weather/SearchActivity.java
+++ weather/app/src/main/java/com/jinaplus/weather/SearchActivity.java
@@ -1,11 +1,12 @@
 package com.jinaplus.weather;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import java.util.Collections;
@@ -16,6 +17,8 @@ import java.util.TreeSet;
  */
 
 public class SearchActivity extends AppCompatActivity {
+    private static final String TAG = SearchActivity.class.getSimpleName();
+
     private WebView webView;
     private TreeSet<String> localAarray = new TreeSet<>();
 
@@ -31,24 +34,27 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         webView = (WebView) findViewById(R.id.wv_search);
+        webView.setWebViewClient(new WebViewClient());
         handleIntent();
     }
 
     private void handleIntent() {
         Intent appLinkIntent = getIntent();
-        Collections.addAll(localAarray, new String[]{"서울", "인천", "부산", "경기", "대구", "대전", "부산", "경주"});
+        Collections.addAll(localAarray, new String[]{"seoul", "incheon", "busan", "kyungki", "deagu", "deajeon"});
         // ATTENTION: This was auto-generated to handle app links.
         String appLinkAction = appLinkIntent.getAction();
-        Uri appLinkData = appLinkIntent.getData();
-        if (appLinkData != null) {
-            String local = appLinkData.getLastPathSegment();
+        String data = appLinkIntent.getDataString();
+        if (Intent.ACTION_VIEW.equals(appLinkAction) && data != null) {
+            String local = data.substring(data.lastIndexOf("/") + 1);
+            Log.i(TAG, "local = " + local);
+            Log.i(TAG, "appLinkIntent.getData() = " + appLinkIntent.getData().toString());
             showWeather(local);
         }
     }
 
     private void showWeather(String local) {
         if(localAarray.contains(local)) {
-            webView.loadUrl("https://www.google.co.kr/search?q=날씨+" + local);
+            webView.loadUrl("https://www.google.co.kr/search?q=weather+" + local);
         } else {
             Toast.makeText(this, local + "은(는) 지원하지 않는 지역입니다.", Toast.LENGTH_SHORT).show();
         }
